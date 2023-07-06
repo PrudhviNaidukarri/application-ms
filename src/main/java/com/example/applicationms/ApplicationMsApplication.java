ac8b6b0211ad086eacaa1a9b34e2fc984f7a471d
Rajesh Kumar <121810307039@gitam.in>
2023-07-06 20:30:35 +0530

    firstCommit

---
+++ src/main/java/com/example/applicationms/ApplicationMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.applicationms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/applicationms/ApplicationMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.applicationms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
